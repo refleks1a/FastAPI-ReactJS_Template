76af239005ee3a6bb7adbd6f876bbd59e4858c13
FedonGN <gnetkov@gmail.com>
2022-02-22 01:47:32 +0300

    add celery

--- backend/app/api/api_v1/endpoints/utils.py
+++ backend/app/api/api_v1/endpoints/utils.py
@@ -6,6 +6,9 @@ from pydantic.networks import EmailStr
 import schemas
 from api import deps
 from utils import send_test_email
+from app.core.celery_app import print_test_message
+from celery.result import AsyncResult
+
 
 router = APIRouter()
 
@@ -19,3 +22,26 @@ def test_email(
     """
     send_test_email(email_to=email_to)
     return {"msg": "Test email sent"}
+
+
+@router.post("/add-test-task/", response_model=schemas.Msg, status_code=201)
+def test_email() -> Any:
+    """
+    Test worker.
+    """
+    task = print_test_message.delay(5)
+    return {"msg": task.id}
+    #return JSONResponse({"task_id": task.id})
+
+
+@router.get("/get-task-info")
+def get_status(
+    task_id: str
+):
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    return result
